4051fa92b59e8dcc16153bdabb9cd34ab7358c56
Дмитрий <dm@dmmmmm.ru>
2019-05-28 16:32:36 +0300

    Task Eniya

--- ABProblem/Program.cs
+++ ABProblem/Program.cs
@@ -11,3 +11,32 @@ namespace ABProblem
         }
     }
 }
+namespace Eniya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            int N;
+            do
+            {
+                A = int.Parse(Console.ReadLine());
+            }
+            while (A < 1 || A > 100);
+            do
+            {
+                B = int.Parse(Console.ReadLine());
+            }
+            while (B < 1 || B > 100);
+            do
+            {
+                N = int.Parse(Console.ReadLine());
+            }
+            while (N < 1 || N > 100);
+            int R = ((A * B) * N) * 2;
+            Console.WriteLine(R);
+        }
+    }
+}
