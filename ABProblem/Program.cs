b2cd140494d0fe742640eeea689dc1277afc062e
Дмитрий <dm@dmmmmm.ru>
2019-05-28 12:53:27 +0300

    Initial commit

---
+++ ABProblem/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ABProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            var s = Console.ReadLine();
+
+            var ss = s.Split(' ');
+
+            var a = int.Parse(ss[0]);
+            var b = int.Parse(ss[1]);
+
+            var r = new Solver.ABSolver(a, b).Solve();
+
+            Console.WriteLine(r);
+        }
+    }
+}
+
+namespace Solver
+{
+    class ABSolver
+    {
+        private int a;
+        private int b;
+
+        public ABSolver(int _a, int _b)
+        {
+            a = _a;
+            b = _b;
+        }
+
+        public int Solve()
+        {
+            return a + b;
+        }
+
+    }
+}
