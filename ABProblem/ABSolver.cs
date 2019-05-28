1d4faafa67f826e6c10a97d84f201176481fd5cf
Дмитрий <dm@dmmmmm.ru>
2019-05-28 12:58:28 +0300

    ABSolver moved to separate file

---
+++ ABProblem/ABSolver.cs
@@ -0,0 +1,20 @@
+﻿namespace Solver
+{
+    class ABSolver
+    {
+        private int a;
+        private int b;
+
+        public ABSolver(int _a, int _b)
+        {
+            a = _a;
+            b = _b;
+        }
+
+        public int Solve()
+        {
+            return a + b;
+        }
+
+    }
+}
--- ABProblem/Program.cs
+++ ABProblem/Program.cs
@@ -15,31 +15,14 @@ namespace ABProblem
             var a = int.Parse(ss[0]);
             var b = int.Parse(ss[1]);
 
-            var r = a + b;//new Solver.ABSolver(a, b).Solve();
+            var r = new Solver.ABSolver(a, b).Solve();
 
             Console.WriteLine(r);
         }
     }
 }
 
-/*
+
 namespace Solver
 {
-    class ABSolver
-    {
-        private int a;
-        private int b;
-
-        public ABSolver(int _a, int _b)
-        {
-            a = _a;
-            b = _b;
-        }
-
-        public int Solve()
-        {
-            return a + b;
-        }
-
-    }
-}*/
+}
