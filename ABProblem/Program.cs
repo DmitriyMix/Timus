6ce98b43bfd4b74679dbfb634a9f7a4fd8f42439
Дмитрий <dm@dmmmmm.ru>
2019-05-28 13:36:38 +0300

    Square root solver moved to separate class

--- ABProblem/Program.cs
+++ ABProblem/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 
 namespace ABProblem
 {
@@ -9,23 +8,6 @@ namespace ABProblem
         {
             var s = Console.In.ReadToEnd();
             SquareRoot.MakeTest(s);
-        }
-    }
-}
-
-class SquareRoot
-{
-    public static void MakeTest(string s)
-    {
-        var ss = s.Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-
-        for (int i = 0; i < ss.Length; ++i)
-        {
-            //Console.WriteLine(ss[ss.Length - i - 1]);
-
-            var x = double.Parse(ss[ss.Length - i - 1]);
-            var y = string.Format(CultureInfo.InvariantCulture, "{0:0.0000}", Math.Sqrt(x));
-            Console.WriteLine(y);
         }
     }
 }
---
+++ ABProblem/SquareRoot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+class SquareRoot
+{
+    public static void MakeTest(string s)
+    {
+        var ss = s.Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < ss.Length; ++i)
+        {
+            var x = double.Parse(ss[ss.Length - i - 1]);
+            var y = string.Format(CultureInfo.InvariantCulture, "{0:0.0000}", Math.Sqrt(x));
+            Console.WriteLine(y);
+        }
+    }
+}
