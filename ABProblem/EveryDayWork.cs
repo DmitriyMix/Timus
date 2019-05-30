29f1a9d0a86f9e5c11a0b71b97218c963d0ee1db
Дмитрий <dm@dmmmmm.ru>
2019-05-30 14:10:55 +0300

    Finish programm

---
+++ ABProblem/EveryDayWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EveryDayWork
+{
+    class Programm
+    {
+        static void Main()
+        {
+            string[] nums = Console.ReadLine().Split(' ');
+            long n = int.Parse(nums[0]);
+            long m = int.Parse(nums[1]);
+            if (0 <= n && n <= 40000)
+            {
+                if (0 <= m && m <= 40000)
+                {
+                    Console.WriteLine(n * (m + 1));
+                }
+            }
+        }
+    }
+}
