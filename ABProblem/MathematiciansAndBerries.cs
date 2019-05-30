948fbf93ec33bbee716c382d8477610b58b259d4
Дмитрий <dm@dmmmmm.ru>
2019-05-30 13:36:57 +0300

    MathematiciansAndBerries

---
+++ ABProblem/MathematiciansAndBerries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MathematiciansAndBerries
+{
+    class Program
+    {
+        private static void Main()
+        {
+            int a1 = int.Parse(Console.ReadLine());
+            int b1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            int b2 = int.Parse(Console.ReadLine());
+            int a3 = int.Parse(Console.ReadLine());
+            int b3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(a1 - a3);
+            Console.WriteLine(b1 - b2);
+        }
+    }
+}
