694bba4bcc88ad908325b06f1320dc657dca2760
Дмитрий <dm@dmmmmm.ru>
2019-05-30 12:25:16 +0300

    Variant 1

---
+++ ABProblem/TwoBandits1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TwoBandits1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] i = Console.ReadLine().Split(' ');
+            Console.WriteLine(int.Parse(i[0]) + int.Parse(i[1]) - int.Parse(i[0]) - 1);
+            Console.WriteLine(int.Parse(i[0]) + int.Parse(i[1]) - int.Parse(i[1]) - 1);
+        }
+    }
+}
