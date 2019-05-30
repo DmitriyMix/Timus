ea90294f8285aef25aa907ce3eaff1f85f6faad6
Дмитрий <dm@dmmmmm.ru>
2019-05-30 13:16:47 +0300

    BicycleCode

---
+++ ABProblem/BicycleCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BikcycleCode
+{
+	class Program
+	{
+        static void Main()
+        {
+            int code1 = int.Parse(Console.ReadLine());
+            int code2 = int.Parse(Console.ReadLine());
+            if (code1 % 2 == 0 || code2 % 2 != 0)
+                Console.WriteLine("yes");
+            else if (code1 % 2 != 0 || code2 % 2 == 0)
+                Console.WriteLine("no");
+        }
+	}
+}
