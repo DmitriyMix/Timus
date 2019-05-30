538f809f573fa89ec65c103473a5ca20305d7e6f
Дмитрий <dm@dmmmmm.ru>
2019-05-30 11:59:49 +0300

    Small change

---
+++ ABProblem/Eniya.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Eniya
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            int A;
+            int B;
+            int N;
+            do
+            {
+                A = int.Parse(Console.ReadLine());
+            }
+            while (A < 1 || A > 100);
+            do
+            {
+                B = int.Parse(Console.ReadLine());
+            }
+            while (B < 1 || B > 100);
+            do
+            {
+                N = int.Parse(Console.ReadLine());
+            }
+            while (N < 1 || N > 100);
+            int R = ((A * B) * N) * 2;
+            Console.WriteLine(R);
+        }
+    }
+}
