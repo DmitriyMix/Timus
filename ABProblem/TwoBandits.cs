f75fcf63c20e2c4f271b4fa7b1aceb5a715abab6
Дмитрий <dm@dmmmmm.ru>
2019-05-30 12:25:35 +0300

    Variant 2

--- ABProblem/TwoBandits.cs
+++ ABProblem/TwoBandits.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace TwoBandits
 {
-   class Program
+    class Program
     {
-        private static void Main(string[] args)
-        {   
-            string[] i = Console.ReadLine().Split(' ');
-            Console.WriteLine(int.Parse(i[0]) + int.Parse(i[1]) - int.Parse(i[0]) - 1);
-            Console.WriteLine(int.Parse(i[0]) + int.Parse(i[1]) - int.Parse(i[0]) - 1);
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] x = s.Split(' ');
+            int H = Convert.ToInt32(x[0]);
+            int L = Convert.ToInt32(x[1]);
+
+            Console.WriteLine((L - 1) + " " + (H - 1));
+
+        
         }
     }
 }
