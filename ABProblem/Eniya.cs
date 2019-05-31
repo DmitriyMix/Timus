4867e45eeb76ade3338b8382fdd98ce769d20572
Дмитрий <dm@dmmmmm.ru>
2019-05-31 15:18:55 +0300

    Transformation

--- ABProblem/Eniya.cs
+++ ABProblem/Eniya.cs
@@ -9,9 +9,17 @@ namespace Eniya
             int n = int.Parse(Console.ReadLine());
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
-            if (1 <= n && n <= 100 && 1 <= a && a <= 100 && 1 <= b && b <= 100)
+            if (1 <= n && n <= 100)
             {
-                Console.WriteLine((a * b) * n * 2);
+                if (1 <= a && a <= 100)
+                {
+                    if(1 <= b && b <= 100)
+                    {
+                        int s = a * b * n * 2;
+                        Console.WriteLine(s);
+                    }
+
+                }                                          
             }
         }
     }
