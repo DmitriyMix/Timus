4f4f1a28ba501ed0960f0bbf5ce656e991ec696a
Дмитрий <dm@dmmmmm.ru>
2019-05-31 13:28:55 +0300

    refactoring

--- ABProblem/Grisha.cs
+++ ABProblem/Grisha.cs
@@ -9,7 +9,7 @@ namespace Grisha
             int f = int.Parse(Console.ReadLine());
             if (1 <= f && f <= 11)
             {
-                int task = ((12 - f) * 45) - 240;
+                var task = ((12 - f) * 45) - 240;
 
                 var s = task > 0 ? "YES" : "NO";
                 Console.WriteLine(s);
