5658a1dac86bf95bf77402697e7a971f697aad0d
Дмитрий <dm@dmmmmm.ru>
2019-05-31 15:16:30 +0300

    ToTheQuestionAboutSports

---
+++ ABProblem/ToTheQuestionAboutSports.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ToTheQuestionAboutSports
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[][] a = new string[n][];
+            for (int i = 0; i <= n; i++)
+            {
+                a[i] = Console.ReadLine().Split(' ');
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int x = i;
+                int y = 0;
+                while (x >= 0 && x < n)
+                {
+                    Console.WriteLine(a[x][y] + " ");
+                    x--;
+                    y++;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int x = n - 1;
+                int y = i + 1;
+                while (y >= 1 && y < n)
+                {
+                    Console.WriteLine(a[x][y] + " ");
+                    x--;
+                    y++;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
