bc177077b0e69a47b89ee45904211f156c98875b
Дмитрий <dm@dmmmmm.ru>
2019-05-31 12:16:22 +0300

    Grisha Task

---
+++ ABProblem/Grisha.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Grisha
+{
+    class Program
+    {
+        static void Main()
+        {
+            int f = int.Parse(Console.ReadLine());
+            if (1 <= f && f <= 11)
+            {
+                if (f > 7)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
+        }
+    }
+
+}
