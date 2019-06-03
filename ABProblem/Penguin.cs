6aab6b9eebcd011b469fb7da7c7be88e0a353862
Дмитрий <dm@dmmmmm.ru>
2019-06-03 13:45:58 +0300

    Task Penguin+

---
+++ ABProblem/Penguin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Penguin
+{
+    //1585+
+    class Program
+    {
+        static void Main()
+        {
+            int Emperor = 0, Little = 0, Macaroni = 0;
+            int max = int.Parse(Console.ReadLine());
+            string[] s = new string[max];
+            for (int t = 0; t <= max - 1; t++)
+            {
+                s[Convert.ToInt32(t)] = (Console.ReadLine());
+            }
+            for (int v = 0; v <= max - 1; v++)
+            {
+                if (s[v] == "Emperor Penguin")
+                {
+                    Emperor++;
+                }
+                if (s[v] == "Macaroni Penguin")
+                {
+                    Macaroni++;
+                }
+                if (s[v] == "Little Penguin")
+                {
+                    Little++;
+                }
+            }
+            if(Emperor > Macaroni && Emperor > Little)
+            {
+                Console.WriteLine("Emperor Penguin");
+            }
+            if (Macaroni > Emperor && Macaroni > Little)
+            {
+                Console.WriteLine("Macaroni Penguin");
+            }
+            if (Little > Emperor && Little > Macaroni)
+            {
+                Console.WriteLine("Little Penguin");
+            }
+        }
+    }
+}
+
+
+
+
+
