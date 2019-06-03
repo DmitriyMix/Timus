3b797200c118cb9246381061c741c702d81a9af1
Дмитрий <dm@dmmmmm.ru>
2019-06-03 15:32:31 +0300

    Time limit exceeded

---
+++ ABProblem/Bayan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace bayan
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] shop = new string[n];
+            int x = 0;
+            for(int i = 0; i< n; i++)
+            {
+                shop[i] = Console.ReadLine();
+            }
+            for (int i = 0; i<shop.Length; i++)
+            {
+                for(int k = i+1; k <shop.Length;k++)
+                {
+                    if(shop[i] == shop[k])
+                    {
+                        x++;
+                        i--;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            Console.WriteLine(x);
+                  
+        }
+    }
+}
