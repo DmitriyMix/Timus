65bfe2c088f8177adfef6a140d7a59affd92272f
Дмитрий <dm@dmmmmm.ru>
2019-06-03 13:10:11 +0300

    Task Summ+

---
+++ ABProblem/Summ.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Summ
+{
+    //1068+
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.In.ReadLine());
+            (int start, int end) = n > 1 ? (1, n) : (n, 1);
+            int sum = 0;
+            for (int i = start; i <= end; i++)
+                sum += i;
+            Console.WriteLine(sum);
+        }
+    }
+}
+
