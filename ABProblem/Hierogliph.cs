a4a169302fbca8863a6b4f0ae6a2b68b835695ba
Дмитрий <dm@dmmmmm.ru>
2019-06-03 15:33:58 +0300

    Wrong answer

---
+++ ABProblem/Hierogliph.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hierogliph
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] a = new string[n];
+            for (int i = 0; i <= n - 1; i++)
+            {
+                a[i] = Console.ReadLine();
+            }
+            string b = Console.ReadLine();
+            foreach (string d in a)
+            {
+                if (d.Substring(1, 1) == b)
+                    Console.WriteLine(d);
+            }
+        }
+    }
+}
+
+    
+
+
