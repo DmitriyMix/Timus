a1ec7a6750472cd928959f48972be0e192e318db
Дмитрий <dm@dmmmmm.ru>
2019-06-11 18:52:31 +0300

    Factorial+

---
+++ ABProblem/Factorial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Factorial
+{
+    //1083+
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int n = int.Parse(input[0]);
+            int l = input[1].Length;
+            int i = 0;
+            int f = 1;
+            int m = 1;
+            while (m >= 1)
+            {
+                f *= m;
+                m = n - (i * l);                
+                i++;
+            }
+            Console.WriteLine(f);
+        }
+    }
+}
