7a67cbed9323c99da82c6b72727ec77db351d83b
Дмитрий <dm@dmmmmm.ru>
2019-06-13 19:45:26 +0300

    HashMap

---
+++ ABProblem/Donald1.cs
@@ -0,0 +1,26 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using Lucene.Net.Support;
+using System;
+using System.Collections.Generic;
+
+namespace Donald1
+{
+    class Program
+    {
+       
+        static void Main()
+        {
+            HashMap<string, int> boxes;
+            boxes = new HashMap<string, int>()
+            {
+                { new int { "Alice", "Ariel", "Aurora", "Phil", "Peter", "Olaf", "Phoebus", "Ralph", "Robin"}}  
+            }
+            // я погуглил про hashmap, не очень разобрался как он работает, и почти на всех сайтах пишут про использование Dictionary 
+
+        }
+    }
+}
+
+
+
+
