273fdb46a65aed88f892db991597b93231d197d1
Дмитрий <dm@dmmmmm.ru>
2019-06-13 15:51:47 +0300

    MathematiciansAndBerries+

--- ABProblem/MathematiciansAndBerries.cs
+++ ABProblem/MathematiciansAndBerries.cs
@@ -2,18 +2,22 @@
 
 namespace MathematiciansAndBerries
 {
+    //2001+
     class Program
     {
         static void Main()
         {
-            int a1 = int.Parse(Console.ReadLine());
-            int b1 = int.Parse(Console.ReadLine());
-            int a2 = int.Parse(Console.ReadLine());
-            int b2 = int.Parse(Console.ReadLine());
-            int a3 = int.Parse(Console.ReadLine());
-            int b3 = int.Parse(Console.ReadLine());
-            Console.WriteLine("1 - " + (a2 - a1 - a3));
-            Console.WriteLine("2 - " + (b1 - b2));
+            string[] values = Console.ReadLine().Split(' ');
+            int a1 = int.Parse(values[0]);
+            int b1 = int.Parse(values[1]);
+            string[] values1 = Console.ReadLine().Split(' ');
+            int a2 = int.Parse(values1[0]);
+            int b2 = int.Parse(values1[1]);
+            string[] values2 = Console.ReadLine().Split(' ');
+            int a3 = int.Parse(values2[0]);
+            int b3 = int.Parse(values2[1]);
+            Console.WriteLine(a1 - a3);
+            Console.WriteLine(b1 - b2);
         }
     }
 }
