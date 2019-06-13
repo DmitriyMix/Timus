2ecdead171d9f95e336fb4436c3233619fdb02c4
Дмитрий <dm@dmmmmm.ru>
2019-06-13 19:41:32 +0300

    Bayan1, wrong answer

---
+++ ABProblem/Bayan1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace Bayan1
+{
+    class Program
+    {
+        static void Main()
+        {
+          
+            var shop = new HashSet<string>
+            {
+                Console.ReadLine()
+            };
+            Console.WriteLine(shop.Distinct().Count());
+
+        }
+    }
+}
