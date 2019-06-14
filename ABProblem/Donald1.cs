a1912e0adf6ef8e929e6ceb19b585580dc25930b
Дмитрий <dm@dmmmmm.ru>
2019-06-14 17:35:03 +0300

    Donald1 - Dictionary

--- ABProblem/Donald1.cs
+++ ABProblem/Donald1.cs
@@ -1,5 +1,5 @@
-﻿using DocumentFormat.OpenXml.Wordprocessing;
-using Lucene.Net.Support;
+﻿using Antlr.Runtime.Misc;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 
@@ -10,13 +10,46 @@ namespace Donald1
        
         static void Main()
         {
-            HashMap<string, int> boxes;
-            boxes = new HashMap<string, int>()
+            int pos = 1;
+            int sum = 0;
+            int n = int.Parse(Console.ReadLine());
+            string name = Console.ReadLine();
+            Dictionary<int, string> box = new Dictionary<int, string>
             {
-                { new int { "Alice", "Ariel", "Aurora", "Phil", "Peter", "Olaf", "Phoebus", "Ralph", "Robin"}}  
+                { 1, "Alice" },
+                { 1, "Ariel" },
+                { 1, "Aurora" },
+                { 1, "Phil" },
+                { 1, "Peter" },
+                { 1, "Olaf" },
+                { 1, "Phoebus" },
+                { 1, "Ralph" },
+                { 1, "Robin" },   
+                
+                { 2, "Bambi" },
+                { 2, "Belle" },
+                { 2, "Bolt" },
+                { 2, "Mulan" },
+                { 2, "Mowgli" },
+                { 2, "Mickey" },
+                { 2, "Silver" },
+                { 2, "Simba" },
+                { 2, "Stitch" }, 
+                
+                { 3, "Dumbo" },
+                { 3, "Genie" },
+                { 3, "Jiminy" },
+                { 3, "Kuzko" },
+                { 3, "Kida" },
+                { 3, "Kenai" },
+                { 3, "Tarzan" },
+                { 3, "Tiana" },
+                { 3, "Winnie" }
+            };        
+            for (int i = 0; i < n; i++)
+            {
+              
             }
-            // я погуглил про hashmap, не очень разобрался как он работает, и почти на всех сайтах пишут про использование Dictionary 
-
         }
     }
 }
