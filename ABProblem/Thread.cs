115a020a51c408b7555691fb0115a72a1aac5e99
Дмитрий <dm@dmmmmm.ru>
2019-06-17 17:03:52 +0300

    Task Thread

---
+++ ABProblem/Thread.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Thread
+{
+    class Program
+    {
+        static void Main()
+        {
+            int r = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string[] arr = Console.ReadLine().Split(' ');
+
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum = Math.Sqrt(double.Parse(arr[0]));
+            }
+            sum += 2 * Math.PI * r * r;
+            Console.WriteLine("{0:F2}", sum);
+        }
+    }
+}
