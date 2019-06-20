a2c39366ce8844d445b56183c22a7798b38fc1e9
Дмитрий <dm@dmmmmm.ru>
2019-06-20 15:37:29 +0300

    Donald1/change

--- ABProblem/Donald1.cs
+++ ABProblem/Donald1.cs
@@ -14,38 +14,42 @@ namespace Donald1
             int sum = 0;
             int n = int.Parse(Console.ReadLine());
             string name = Console.ReadLine();
-            Dictionary<int, string> box = new Dictionary<int, string>
+
+
+            Dictionary<string, int> box1 = new Dictionary<string, int>
             {
-                { 1, "Alice" },
-                { 1, "Ariel" },
-                { 1, "Aurora" },
-                { 1, "Phil" },
-                { 1, "Peter" },
-                { 1, "Olaf" },
-                { 1, "Phoebus" },
-                { 1, "Ralph" },
-                { 1, "Robin" },   
-                
-                { 2, "Bambi" },
-                { 2, "Belle" },
-                { 2, "Bolt" },
-                { 2, "Mulan" },
-                { 2, "Mowgli" },
-                { 2, "Mickey" },
-                { 2, "Silver" },
-                { 2, "Simba" },
-                { 2, "Stitch" }, 
-                
-                { 3, "Dumbo" },
-                { 3, "Genie" },
-                { 3, "Jiminy" },
-                { 3, "Kuzko" },
-                { 3, "Kida" },
-                { 3, "Kenai" },
-                { 3, "Tarzan" },
-                { 3, "Tiana" },
-                { 3, "Winnie" }
-            };        
+                { "Alice", 1},
+                {"Ariel", 1 },
+                {"Aurora", 1 },
+                {"Phil", 1 },
+                {"Peter", 1 },
+                {"Olaf", 1 },
+                {"Phoebus", 1 },
+                {"Ralph", 1 },
+                {"Robin", 1 },
+
+                { "Bambi", 2 },
+                {  "Belle", 2 },
+                {  "Bolt", 2 },
+                {  "Mulan", 2 },
+                {  "Mowgli", 2 },
+                {  "Mickey", 2 },
+                {  "Silver", 2 },
+                {  "Simba", 2 },
+                {  "Stitch", 2 },
+
+                {  "Dumbo", 3 },
+                {  "Genie", 3 },
+                {  "Jiminy", 3 },
+                {  "Kuzko", 3 },
+                { "Kida", 3 },
+                { "Kenai", 3 },
+                {  "Tarzan", 3 },
+                {  "Tiana", 3 },
+                {  "Winnie", 3 }
+        };
+            var s = "Winnie";
+            var boxNumber = box1[s];
             for (int i = 0; i < n; i++)
             {
               
