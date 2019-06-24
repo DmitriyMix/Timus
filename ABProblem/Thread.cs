80ed333ae568375f848e8c37b3bdc8aff1230462
Дмитрий <dm@dmmmmm.ru>
2019-06-24 17:42:15 +0300

    Thread/ wrong answer test 2

--- ABProblem/Thread.cs
+++ ABProblem/Thread.cs
@@ -18,7 +18,6 @@ namespace Thread
         {
 
             string[] arr = Console.ReadLine().Split(' ');
-            double j = 0.49;
             int n = int.Parse(arr[0]);
             int r = int.Parse(arr[1]);
             int value = 2;
@@ -28,19 +27,21 @@ namespace Thread
             {
                 arr = Console.ReadLine().Split(' ');
                 c[i] = new Coords(double.Parse(arr[0]), double.Parse(arr[1]));
-                          
-            }          
+
+            }
             foreach (var m in c)
             {
-                var l = (m.x - m.y);
-                var h = (m.y - m.x);
-                var k = Math.Pow(l, value);
-                var v = Math.Pow(h, value);
-                var f = k + v;
-                var u = Math.Sqrt(l*l+h*h);//(f)
-
-                //Console.WriteLine(u);
-                sum = u + (2 * Math.PI *r);             
+                var l = (m.x - m.x);
+                var t = (m.x - m.x);
+                var b = (m.y - m.y);
+                var z = (m.y - m.y);
+                var g = Math.Pow(value, t);
+                var h = Math.Pow(value, z);
+                var a = Math.Pow(value, l);                                                                             
+                var p = Math.Pow(value, b);                
+                var f = p + a + g + h;
+                var u = Math.Sqrt(f);//(l*l+h*h);                
+                sum = 4 * u + (2 * Math.PI * r);
             }
             Console.WriteLine("{0:F2}", sum);
         }
